c4f1e399890b690f8b96b84c93fc5a9fc11be611
Ekateryna Gruvman <kate.gruvman@ukr.net>
2022-06-20 13:54:29 +0200

    add form

---
+++ src/components/add-form/add-form.jsx
@@ -0,0 +1,22 @@
+import "../add-form/add-form.css";
+
+const AddForm = () => {
+  return (
+    <div className="app-add-form">
+      <h3>Ad file</h3>
+      <form className="add-form d-flex">
+        <input
+          type="text"
+          className="form-control new-post-label"
+          placeholder="please upload file here"
+        />
+
+        <button type="submit" className="btn btn-outline-light">
+          ADD
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddForm;
---
+++ src/components/app/app.jsx
@@ -0,0 +1,16 @@
+import Header from "../header/header";
+
+import AddForm from "../add-form/add-form";
+
+import "./app.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <AddForm />
+    </div>
+  );
+}
+
+export default App;
